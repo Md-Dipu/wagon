d43b4cf07a026e62e27322efb25206b72bac6010
Md. Dipu <srabun.tangail@gmail.com>
2022-01-18 23:33:32 +0600

    home page with banner

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../HomeBanner/HomeBanner';
+
+const Home = () => {
+    document.title = "Wagon - Buy your Dream Apartment";
+
+    return (
+        <>
+            <Banner />
+        </>
+    );
+};
+
+export default Home;
