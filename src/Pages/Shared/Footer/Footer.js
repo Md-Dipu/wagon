e407826384646a457f2adc6d81e3f04b8b1bfeed
Md. Dipu <srabun.tangail@gmail.com>
2022-01-18 23:33:12 +0600

    simple footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className="bg-dark text-center text-light mb-0 py-1">Copyright&copy;2022-2025 | Wagon</p>
+        </div>
+    );
+};
+
+export default Footer;
