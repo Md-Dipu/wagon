ad2a76a0adf209c747a126b116f6fb2534151322
Md. Dipu <srabun.tangail@gmail.com>
2022-01-19 16:40:13 +0600

    modify button styles

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -7,15 +7,15 @@ const Navigation = () => {
     return (
         <Navbar variant="dark" expand="lg" className="niche-navbar">
             <Container>
-                <Navbar.Brand as={NavLink} to="/">Wagon</Navbar.Brand>
+                <Navbar.Brand as={NavLink} to="/" className="fw-bold">Wagon</Navbar.Brand>
                 <Navbar.Toggle aria-controls="niche-navbar-nav" />
                 <Navbar.Collapse id="niche-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/about-us">About Us</Nav.Link>
-                        <Nav.Link as={NavLink} to="/units">Units</Nav.Link>
-                        <Nav.Link as={NavLink} to="/reviews">Reviews</Nav.Link>
-                        <Nav.Link as={NavLink} to="/contact-us">Contact Us</Nav.Link>
+                        <Nav.Link as={NavLink} to="/" className="fw-bold">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about-us" className="fw-bold">About Us</Nav.Link>
+                        <Nav.Link as={NavLink} to="/units" className="fw-bold">Units</Nav.Link>
+                        <Nav.Link as={NavLink} to="/reviews" className="fw-bold">Reviews</Nav.Link>
+                        <Nav.Link as={NavLink} to="/contact-us" className="fw-bold">Contact Us</Nav.Link>
                     </Nav>
                     <div>
                         <Button as={NavLink} to="/login" variant="light" className="rounded-pill niche-nav-btn">Login</Button>{" "}
