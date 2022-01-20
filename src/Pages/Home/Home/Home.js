64974a1d0eed77ee939718f3f9ebe7035d262432
Md. Dipu <srabun.tangail@gmail.com>
2022-01-20 23:43:01 +0600

    home page with nav and others components

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import About from '../AboutSummary/AboutSummary';
+import Apartments from '../HomeApartments/HomeApartments';
 import Banner from '../HomeBanner/HomeBanner';
+import Reviews from '../HomeReviews/HomeReviews';
 
 const Home = () => {
     document.title = "Wagon - Buy your Dream Apartment";
 
     return (
         <>
+            <Navigation />
             <Banner />
+            <About />
+            <Apartments />
+            <Reviews />
         </>
     );
 };
