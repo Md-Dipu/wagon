88638f71a8c9dfcd069f00d37d210a710a6affa8
Md. Dipu <srabun.tangail@gmail.com>
2022-01-20 23:40:18 +0600

    created apartment card

---
+++ src/Pages/Shared/Apartment/Apartment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Apartment = (props) => {
+    const { apartment } = props;
+    const { _id, name, img, description, price } = apartment;
+    const { shortDescription } = description;
+    return (
+        <Card className="border-0">
+            <Card.Img variant="top" src={img} alt={`Picture of ${name}`} />
+            <Card.Body className="p-0 mt-2">
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{shortDescription}</Card.Text>
+                <span className="niche-divider opacity-50"></span>
+                <div className="d-flex justify-content-between mt-2">
+                    <Card.Text as="p" className="h3">${price}</Card.Text>
+                    <Button as={Link} to={`/apartments/${_id}`} variant="warning" className="text-uppercase">buy</Button>
+                </div>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Apartment;
