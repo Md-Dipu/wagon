a642d8628f84865b093ef9affda12fb0e74fea3a
Md. Dipu <srabun.tangail@gmail.com>
2022-01-20 20:08:30 +0600

    HashLink replaced with Link

--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import './HomeBanner.css';
+import { HashLink } from 'react-router-hash-link';
 
 const HomeBanner = () => {
     return (
@@ -11,8 +12,8 @@ const HomeBanner = () => {
                     Awesome Apartments <br />
                     are waiting for you.
                 </h1>
-                <Button as={Link} to="/units" variant="primary" className="rounded-pill niche-home-banner-btn">Buy One</Button>{" "}
-                <Button as={Link} to="/contact-us" variant="outline-light" className="rounded-pill niche-home-banner-btn">Contact Us</Button>
+                <Button as={Link} to="/apartments" variant="primary" className="rounded-pill niche-home-banner-btn">Buy One</Button>{" "}
+                <Button as={HashLink} to="/#contact-us" variant="outline-light" className="rounded-pill niche-home-banner-btn">Contact Us</Button>
             </div>
         </div>
     );
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import './Navigation.css';
 
@@ -11,11 +12,11 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls="niche-navbar-nav" />
                 <Navbar.Collapse id="niche-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link as={NavLink} to="/" className="fw-bold">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/about-us" className="fw-bold">About Us</Nav.Link>
-                        <Nav.Link as={NavLink} to="/units" className="fw-bold">Units</Nav.Link>
-                        <Nav.Link as={NavLink} to="/reviews" className="fw-bold">Reviews</Nav.Link>
-                        <Nav.Link as={NavLink} to="/contact-us" className="fw-bold">Contact Us</Nav.Link>
+                        <Nav.Link as={NavLink} exact to="/" className="fw-bold">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/#about-us" className="fw-bold">About Us</Nav.Link>
+                        <Nav.Link as={NavLink} exact to="/apartments" className="fw-bold">Apartments</Nav.Link>
+                        <Nav.Link as={HashLink} to="/#reviews" className="fw-bold">Reviews</Nav.Link>
+                        <Nav.Link as={HashLink} to="/#contact-us" className="fw-bold">Contact Us</Nav.Link>
                     </Nav>
                     <div>
                         <Button as={NavLink} to="/login" variant="light" className="rounded-pill niche-nav-btn">Login</Button>{" "}
