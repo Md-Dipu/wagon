2ee7c814787a7adf8cd1b4579c519e430e44b00b
Md. Dipu <srabun.tangail@gmail.com>
2022-01-20 23:43:39 +0600

    apartments route added and navigation reomoved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Apartments from "./Pages/Apartments/Apartments/Apartments";
 import Home from "./Pages/Home/Home/Home";
 import Footer from "./Pages/Shared/Footer/Footer";
-import Navigation from "./Pages/Shared/Navigation/Navigation";
 
 const App = () => {
     return (
         <Router>
-            <Navigation />
             <Switch>
                 <Route exact path="/">
                     <Home />
+                </Route>
+                <Route path="/apartments">
+                    <Apartments />
                 </Route>
             </Switch>
             <Footer />
