27207877fa028a159fe7dde8c06bf9374ddf96ee
Md. Dipu <srabun.tangail@gmail.com>
2022-01-24 00:19:28 +0600

    created login form

---
+++ src/Pages/Authentication/LoginForm/LoginForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const handleOnSubmit = data => {
+        console.log(data);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(handleOnSubmit)}>
+            <Form.Group className="mb-3">
+                <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    className={`${errors.email && 'border-danger'}`}
+                    {...register('email', { required: true })}
+                />
+            </Form.Group>
+            <Form.Group className="mb-1">
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    className={`${errors.password && 'border-danger'}`}
+                    {...register('password', { required: true, minLength: 6 })}
+                />
+            </Form.Group>
+            <Button type="button" variant="link" as={Link} to="/register" className="mb-1 text-decoration-none">Don't have an account?</Button><br />
+            <Button type="submit">Login</Button>{" "}
+            <Button type="button" variant="light" onClick={() => reset()}>Cancel</Button>
+        </form>
+    );
+};
+
+export default LoginForm;
