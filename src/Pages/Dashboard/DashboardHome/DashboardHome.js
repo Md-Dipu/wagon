040f9dc12cee6bc36be180d0eb2ba49555b4e7ad
Md. Dipu <srabun.tangail@gmail.com>
2022-01-23 14:35:40 +0600

    created dashboard home

---
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardHome = () => {
+    return (
+        <div className='p-3'>
+            This is Dashboard
+        </div>
+    );
+};
+
+export default DashboardHome;
