87ed007c13598abdf9d869f0916ea64a76599d06
Md. Dipu <srabun.tangail@gmail.com>
2022-01-24 00:18:31 +0600

    alternative navbar

---
+++ src/Pages/Shared/DarkNav/DarkNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+const DarkNav = (props) => {
+    return (
+        <>
+            <Navigation {...props} />
+            <div className="bg-dark d-lg-block d-none" style={{ height: '56px' }}></div>
+        </>
+    );
+};
+
+export default DarkNav;
