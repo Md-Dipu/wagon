9fec57a96763052c0c6acceb722fa4f2b024ec3b
Md. Dipu <srabun.tangail@gmail.com>
2022-01-23 14:22:17 +0600

    dashboard off canvas

---
+++ src/Pages/Dashboard/DashboardMobileOffCanvas/DashBoardMobileOffCanvas.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Offcanvas } from 'react-bootstrap';
+import DashboardSideNav from '../DashboardSideNav/DashboardSideNav';
+
+const DashBoardMobileOffCanvas = ({ show, handleClose, url }) => {
+    return (
+        <Offcanvas show={show} onHide={handleClose} className="bg-secondary">
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title as={Link} to="/" className="text-decoration-none text-white fw-bold fs-3">Wagon</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <DashboardSideNav
+                    url={url}
+                    closeOffCanvas={handleClose}
+                />
+            </Offcanvas.Body>
+        </Offcanvas>
+    );
+};
+
+export default DashBoardMobileOffCanvas;
