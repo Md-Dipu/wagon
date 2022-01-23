b5e678df60f607383d7ab9596968ce4bb6b59b05
Md. Dipu <srabun.tangail@gmail.com>
2022-01-24 00:56:40 +0600

    register new user

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import initializeFirebase from '../Pages/Authentication/Firebase/firebase.init';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 // initialize firebase app
 initializeFirebase();
@@ -24,7 +24,24 @@ const useFirebase = () => {
         return () => unsubscribe;
     }, [auth]);
 
-    return { user };
+    // register user
+    const register = (newUser) => {
+        const { name, email, password, phone } = newUser;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                // update user information to firebase
+                updateProfile(auth.currentUser, { displayName: name, phone })
+                    .then(() => {
+                        // data updated
+                    })
+                    .catch(error => {
+                        throw new Error(error)
+                    });
+            })
+            .catch(console.error);
+    }
+
+    return { user, register };
 };
 
 export default useFirebase;
