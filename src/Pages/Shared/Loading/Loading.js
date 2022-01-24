2a1fc74d9215696c56f69055cac0da59b4a81844
Md. Dipu <srabun.tangail@gmail.com>
2022-01-24 21:05:15 +0600

    created Loading component

---
+++ src/Pages/Shared/Loading/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loading = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+            <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </div>
+    );
+};
+
+export default Loading;
