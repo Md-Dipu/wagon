e4afb52b979277b08c25377260bd18119aa8f9c7
Md. Dipu <srabun.tangail@gmail.com>
2022-01-24 12:09:45 +0600

    save new user to database

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -26,19 +26,38 @@ const useFirebase = () => {
 
     // register user
     const register = (newUser) => {
-        const { name, email, password, phone } = newUser;
+        const { name, email, password } = newUser;
         createUserWithEmailAndPassword(auth, email, password)
             .then(() => {
                 // update user information to firebase
-                updateProfile(auth.currentUser, { displayName: name, phone })
+                updateProfile(auth.currentUser, { displayName: name })
                     .then(() => {
                         // data updated
                     })
                     .catch(error => {
                         throw new Error(error)
                     });
+
+                // update user information to website database
+                const newUserData = { ...newUser };
+                delete newUserData.password;
+                saveUser(newUserData);
             })
             .catch(console.error);
+    }
+
+    // save user
+    const saveUser = (newUser) => {
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .catch(error => {
+                throw new Error(error);
+            });
     }
 
     // login user
