62f0e384954d135c208c118318c88e1415a4f18e
Md. Dipu <srabun.tangail@gmail.com>
2022-01-24 23:13:01 +0600

    admin only links added

--- src/Pages/Dashboard/DashboardSideNav/DashboardSideNav.js
+++ src/Pages/Dashboard/DashboardSideNav/DashboardSideNav.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ListGroup } from 'react-bootstrap';
 import './DashboardSideNav.css';
+import useAuth from '../../../Hooks/useAuth';
 
 const DashboardSideNav = ({ url, closeOffCanvas = () => { } }) => {
+    const { admin } = useAuth(); console.log(admin);
+
     return (
         <ListGroup>
             <ListGroup.Item as={Link} to={url} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Dashboard</ListGroup.Item>
             <ListGroup.Item as={Link} to={`${url}/my-orders`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">My Orders</ListGroup.Item>
             <ListGroup.Item as={Link} to={`${url}/my-reviews`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">My Reviews</ListGroup.Item>
             <ListGroup.Item as={Link} to={`${url}/pay-now`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Pay Now</ListGroup.Item>
+
+            {/* admin only links */}
+            {admin && <>
+                <ListGroup.Item as={Link} to={`${url}/manage-all-orders`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Manage All Orders</ListGroup.Item>
+                <ListGroup.Item as={Link} to={`${url}/add-a-product`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Add A Product</ListGroup.Item>
+                <ListGroup.Item as={Link} to={`${url}/manage-products`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Manage Products</ListGroup.Item>
+                <ListGroup.Item as={Link} to={`${url}/make-admin`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Make Admin</ListGroup.Item>
+            </>}
         </ListGroup>
     );
 };
