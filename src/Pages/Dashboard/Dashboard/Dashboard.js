1b63da5a63d604bbc10af9573ea7e2b3a01a5ca1
Md. Dipu <srabun.tangail@gmail.com>
2022-01-26 20:24:48 +0600

    added make-admin route and redircet setup

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
+import useAuth from '../../../Hooks/useAuth';
+import redirect from '../../../Utilities/redirect';
 import Home from '../DashboardHome/DashboardHome';
 import SideNav from '../DashboardSideNav/DashboardSideNav';
 import TopNav from '../DashboardTopNav/DashboardTopNav';
 import MobileOffCanvas from '../DashboardMobileOffCanvas/DashBoardMobileOffCanvas';
+import MakeAdmin from '../MakeAdmin/MakeAdmin';
+import AdminRoute from '../../Authentication/AdminRoute/AdminRoute';
 import './Dashboard.css';
 
 const Dashboard = () => {
     const [showMobileOffCanvas, setShowMobileOffCanvas] = React.useState(false);
 
     const { path, url } = useRouteMatch();
+    const { admin } = useAuth();
+
+    const location = useLocation();
+    const history = useHistory();
+
+    // redirect
+    if (admin && Boolean(location?.state?.from)) {
+        redirect(history, location?.state?.from, '/dashboard');
+    }
 
     return (
         <>
@@ -22,14 +35,17 @@ const Dashboard = () => {
             />
             <Container fluid>
                 <Row>
-                    <Col xs={12} md={3} lg={2} className="d-lg-block d-none bg-secondary niche-dashboard-lg-side-nav">
+                    <Col xs={12} lg={2} className="d-lg-block d-none bg-secondary niche-dashboard-lg-side-nav">
                         <SideNav url={url} />
                     </Col>
-                    <Col xs={12} md={9} lg={10} className="p-0">
+                    <Col xs={12} lg={10} className="p-0">
                         <Switch>
                             <Route exact path={path}>
                                 <Home />
                             </Route>
+                            <AdminRoute path={`${path}/make-admin`}>
+                                <MakeAdmin />
+                            </AdminRoute>
                         </Switch>
                     </Col>
                 </Row>
