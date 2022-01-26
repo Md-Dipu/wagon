d919f90f33af406ed837d3f2166f587086342f58
Md. Dipu <srabun.tangail@gmail.com>
2022-01-27 01:31:48 +0600

    Edited link text

--- src/Pages/Dashboard/DashboardSideNav/DashboardSideNav.js
+++ src/Pages/Dashboard/DashboardSideNav/DashboardSideNav.js
@@ -10,7 +10,7 @@ const DashboardSideNav = ({ url, closeOffCanvas = () => { } }) => {
     return (
         <ListGroup>
             <ListGroup.Item as={Link} to={url} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Dashboard</ListGroup.Item>
-            <ListGroup.Item as={Link} to={`${url}/my-bookings`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">My Booking</ListGroup.Item>
+            <ListGroup.Item as={Link} to={`${url}/my-bookings`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">My Bookings</ListGroup.Item>
             <ListGroup.Item as={Link} to={`${url}/my-reviews`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">My Reviews</ListGroup.Item>
             <ListGroup.Item as={Link} to={`${url}/pay-now`} onClick={closeOffCanvas} className="niche-dashboard-side-nav-item">Pay Now</ListGroup.Item>
 
