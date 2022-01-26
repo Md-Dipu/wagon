709597c6708ff03989917fae3e5549f765995c85
Md. Dipu <srabun.tangail@gmail.com>
2022-01-26 20:22:45 +0600

    created MakeAdmin with AdminTable

---
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button, Container, Form, InputGroup } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import AdminTable from '../AdminTable/AdminTable';
+
+const MakeAdmin = () => {
+    const [stateObsever, setStateObserver] = React.useState(0); // number: observeing change every time
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = data => {
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                reset();
+                if (data.modifiedCount > 0)
+                    setStateObserver(stateObsever + 1);
+            })
+            .catch(console.error);
+    }
+
+    return (
+        <>
+            <Container fluid className="my-4">
+                <h4 className="text-center text-uppercase mb-3">make new admin</h4>
+                <form onSubmit={handleSubmit(onSubmit)} className="mx-auto" style={{ maxWidth: 500 }}>
+                    <InputGroup className="">
+                        <Form.Control
+                            type="email"
+                            placeholder="Email"
+                            className={errors.email && 'border-danger'}
+                            {...register('email', { required: true })}
+                        />
+                        <Button
+                            type="submit"
+                            variant="secondary"
+                        >
+                            Add as Admin
+                        </Button>
+                    </InputGroup>
+                </form>
+            </Container>
+            <AdminTable
+                observer={stateObsever}
+            />
+        </>
+    );
+};
+
+export default MakeAdmin;
