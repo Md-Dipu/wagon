e5c5659891d6f2be20258c45d884a29903c3173c
Md. Dipu <srabun.tangail@gmail.com>
2022-01-27 21:01:34 +0600

    Created PayNow with text coming soon

---
+++ src/Pages/Dashboard/PayNow/PayNow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const PayNow = () => {
+    return (
+        <Container fluid className="py-3">
+            <h5>Payment system coming soon</h5>
+        </Container>
+    );
+};
+
+export default PayNow;
