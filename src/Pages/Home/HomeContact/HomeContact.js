cf248a10bdaa8190d3a8c391996fc2b44a39ecb3
Md. Dipu <srabun.tangail@gmail.com>
2022-01-27 14:34:39 +0600

    Created HomeContact with bg-image

---
+++ src/Pages/Home/HomeContact/HomeContact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeContact.css';
+
+const HomeContact = () => {
+    return (
+        <div id="contact-us" className="niche-home-contact">
+            this is contact us form
+        </div>
+    );
+};
+
+export default HomeContact;
