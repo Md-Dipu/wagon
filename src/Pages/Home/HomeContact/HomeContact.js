010001edbf5790504258cf0f7b52eb65b73ac15d
Md. Dipu <srabun.tangail@gmail.com>
2022-01-27 15:28:53 +0600

    padding and margin changes

--- src/Pages/Home/HomeContact/HomeContact.js
+++ src/Pages/Home/HomeContact/HomeContact.js
@@ -4,10 +4,10 @@ import { Button, Container, Col, Form, Row } from 'react-bootstrap';
 
 const HomeContact = () => {
     return (
-        <div id="contact-us">
+        <div id="contact-us" className="py-5">
             <Container>
                 <Row className="justify-content-center align-items-center">
-                    <Col xs={12} sm={10} md={6} lg={5} className='my-5'>
+                    <Col xs={12} sm={10} md={6} lg={5}>
                         <form className="form-dark text-center rounded shadow p-4">
                             <h5 className="text-uppercase mb-3">Contact Us</h5>
                             <Form.Control
@@ -30,10 +30,17 @@ const HomeContact = () => {
                             <Button type="submit">Send</Button>
                         </form>
                     </Col>
-                    <Col xs={12} sm={10} md={6} lg={5} className='my-5'>
+                    <Col xs={12} sm={10} md={6} lg={5} className="mt-md-0 mt-4">
                         <h5>Support</h5>
                         <p className="fs-6">Our support team always ready to help you. So, don't waste your time with problems. Just send a message and our team will to solve the problem. Enjoy you service.</p>
-                        <Button variant="outline-secondary" as={Link} to="/apartments" className="rounded-pill">Buy One Now</Button>
+                        <Button
+                            variant="outline-secondary"
+                            as={Link}
+                            to="/apartments"
+                            className="rounded-pill"
+                        >
+                            Buy One Now
+                        </Button>
                     </Col>
                 </Row>
             </Container>
