26f1082e39c07daf900413f0a8d39977082e25c7
Md. Dipu <srabun.tangail@gmail.com>
2022-01-28 16:26:56 +0600

    get reviews from server and modified based on data

--- src/Pages/Home/HomeReviews/HomeReviews.js
+++ src/Pages/Home/HomeReviews/HomeReviews.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './HomeReviews.css';
 
-const reviews = [
-    { name: 'Dowam Mehahil', reviewText: 'Of couse the best online apartment saler website. Attached companies and Provieders are well knowned and helpfull. They help me too much and provied me a best suggestion to find-out my choice.' },
-    { name: 'H. Barmand', reviewText: 'I had a great time when I was dealing with this people about my case. I was looking for a good looking apartment what should be located in a soundless place. I found the best one for me.' }
-];
-
 const HomeReviews = () => {
+    const [reviews, setReviews] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('http://localhost:5000/reviews')
+            .then(res => res.json())
+            .then(setReviews)
+            .catch(console.error);
+    }, []);
+
     return (
         <Container className="niche-home-reviews my-4 pt-sm-4" id="reviews">
             <Carousel controls={false} indicators={false} className="niche-home-carosel ms-sm-4 bg-light px-4 py-3">
-                {reviews.map((review, _idx) => (
-                    <Carousel.Item key={_idx}>
-                        <p className="fs-6 fst-italic">"{review.reviewText}"</p>
+                {reviews.map(review =>
+                    <Carousel.Item key={review._id}>
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <FontAwesomeIcon icon={faStar} color="#ffd700" /> {review.rating}
+                            </div>
+                            <p className="fw-bold text-secondary">{review.ratingCodeWord}</p>
+                        </div>
+                        <p className="fs-6 fst-italic">"{review.message}"</p>
                         <p className="h6 text-end">-{review.name}</p>
                     </Carousel.Item>
-                ))}
+                )}
             </Carousel>
         </Container>
     );
