7b0dc6e8d009c152cf8c32388584be56d4757df4
Md. Dipu <srabun.tangail@gmail.com>
2022-01-29 11:44:13 +0600

    Created NotFound page

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { HashLink } from 'react-router-hash-link';
+import { Button } from 'react-bootstrap';
+import DarkNav from '../Shared/DarkNav/DarkNav';
+import Footer from '../Shared/Footer/Footer';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <>
+            <DarkNav />
+            <div className="niche-not-found-page">
+                <div className="text-center">
+                    <p className="display-3 fw-bold">404</p>
+                    <p className="fs-3 fw-bold">Page Not Found</p>
+                    <Button as={HashLink} to="/#" variant="light" size="lg" className="rounded-pill">Back to Home</Button>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default NotFound;
