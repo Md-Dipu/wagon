018927d949f138d902c4a827ce4f6f97ec2967a4
Md. Dipu <srabun.tangail@gmail.com>
2023-02-14 15:31:03 +0600

    [add route] users route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 
 const apartmentRoutes = require("./routes/apartmentRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/apartments", apartmentRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).json({
