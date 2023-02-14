16826e56937ba487f0d3c75963f6a386d4040713
Md. Dipu <srabun.tangail@gmail.com>
2023-02-14 20:12:13 +0600

    [add service] find bookings with filters and queries

--- server/services/bookingServices.js
+++ server/services/bookingServices.js
@@ -4,3 +4,16 @@ exports.insertBooking = async (data) => {
     const result = Booking.create(data);
     return result;
 };
+
+exports.findBookings = async (filters, queries) => {
+    const bookings = await Booking.find(filters)
+        .limit(queries.limit)
+        .select(queries.fields)
+        .skip(queries.skip)
+        .sort(queries.sortby);
+    const count = await Booking.countDocuments(filters);
+    return {
+        users: bookings,
+        count
+    };
+};
