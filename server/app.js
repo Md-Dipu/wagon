b7c614db2707dc8677a3169a5b28ad0b445bf3b9
Md. Dipu <srabun.tangail@gmail.com>
2023-02-14 14:17:43 +0600

    [add route] apartment route

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const cors = require("cors");
 const express = require("express");
 
+const apartmentRoutes = require("./routes/apartmentRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/apartments", apartmentRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).json({
