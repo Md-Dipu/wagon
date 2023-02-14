2703d18831627f6c0afa2c77f71cf4f05f89e116
Md. Dipu <srabun.tangail@gmail.com>
2023-02-14 20:12:32 +0600

    [add api] get bookings

--- server/controllers/bookingControllers.js
+++ server/controllers/bookingControllers.js
@@ -1,4 +1,5 @@
-const { insertBooking } = require("../services/bookingServices");
+const { insertBooking, findBookings } = require("../services/bookingServices");
+const parseQuery = require("../utils/parseQuery");
 
 exports.postBooking = async (req, res) => {
     try {
@@ -16,6 +17,28 @@ exports.postBooking = async (req, res) => {
         res.status(500).json({
             success: false,
             message: "Couldn't able to insert booking data",
+            error: error.message
+        });
+    }
+};
+
+exports.getBookings = async (req, res) => {
+    const { filters, queries } = parseQuery(req.query);
+
+    try {
+        const result = await findBookings(filters, queries);
+
+        res.status(200).json({
+            success: true,
+            message: "Users data found successfully",
+            data: result.users,
+            count: result.count
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't able to find users data",
             error: error.message
         });
     }
--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -4,6 +4,8 @@ const bookingController = require("../controllers/bookingControllers");
 
 const router = express.Router();
 
-router.route("/").post(bookingController.postBooking);
+router.route("/")
+    .post(bookingController.postBooking)
+    .get(bookingController.getBookings);
 
 module.exports = router;
