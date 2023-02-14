23fd53f3b82b4f368f7eae1be2fbb5bcb9ef5242
Md. Dipu <srabun.tangail@gmail.com>
2023-02-14 14:47:56 +0600

    [add func] query processor function

---
+++ server/utils/parseQuery.js
@@ -0,0 +1,41 @@
+const renameMongoOperatorsKeys = (obj) => {
+    const operators = "all and elemMatch eq exists expr gt gte in jsonSchema lt lte meta mod ne nin nor not or regex size slice text type where";
+    for (const key in obj) {
+        if (Object.hasOwnProperty.call(obj, key)) {
+            if (typeof obj[key] === "object") {
+                renameMongoOperatorsKeys(obj[key]);
+            }
+            if (operators.split(" ").includes(key)) {
+                const newKey = `$${key}`;
+                obj[newKey] = obj[key];
+                delete obj[key];
+            }
+        }
+    }
+};
+
+module.exports = (query) => {
+    const { fields, limit, page, sortby, ...filters } = query;
+
+    // processing queries object
+    renameMongoOperatorsKeys(filters);
+
+    // processing queries object
+    const queries = {};
+
+    if (fields) {
+        queries.fields = fields.split(",").join(" ");
+    }
+
+    queries.limit = parseInt(limit) || 10;
+    queries.skip = queries.limit * ((parseInt(page) || 1) - 1);
+
+    if (sortby) {
+        queries.sortby = sortby.split(",").join(" ");
+    }
+
+    return {
+        filters,
+        queries
+    };
+};
