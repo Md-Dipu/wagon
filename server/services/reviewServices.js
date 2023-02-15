5753d088d8a7b386fe1b3f990c4240b402533824
Md. Dipu <srabun.tangail@gmail.com>
2023-02-15 15:21:43 +0600

    [add service] find reviews

--- server/services/reviewServices.js
+++ server/services/reviewServices.js
@@ -4,3 +4,16 @@ exports.insertReview = async (data) => {
     const result = Review.create(data);
     return result;
 };
+
+exports.findReviews = async (filters, queries) => {
+    const reviews = await Review.find(filters)
+        .limit(queries.limit)
+        .select(queries.fields)
+        .skip(queries.skip)
+        .sort(queries.sortby);
+    const count = await Review.countDocuments(filters);
+    return {
+        reviews,
+        count
+    };
+};
