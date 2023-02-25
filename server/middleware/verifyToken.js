887483bbe1efd5ea68ff07f9f64688412cb72c66
Md. Dipu <srabun.tangail@gmail.com>
2023-02-25 22:09:25 +0600

    [add middleware] verify token

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,35 @@
+const admin = require("firebase-admin");
+
+const { findUserByEmail } = require("../services/userServices");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const verifyToken = async (req, res, next) => {
+    try {
+        if (!req.headers?.authorization?.startsWith("Bearer ")) {
+            throw new Error("Bearer token not found");
+        }
+
+        const idToken = req.headers.authorization.split(" ")[1];
+        const decodedUser = await admin.auth().verifyIdToken(idToken);
+        const user = await findUserByEmail(decodedUser.email);
+        req.user = {
+            ...decodedUser,
+            role: user.role
+        };
+        next();
+
+    } catch (error) {
+        res.status(401).json({
+            success: false,
+            message: "User is not authorized",
+            error: error.message
+        });
+    }
+};
+
+module.exports = verifyToken;
