678450fbc471dd5fecff02fd6a06b0ed15d15398
Md. Dipu <srabun.tangail@gmail.com>
2023-02-26 22:08:40 +0600

    [add] verify post review

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const reviewControllers = require("../controllers/reviewControllers");
+const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
 
 router.route("/")
-    .post(reviewControllers.postReview)
+    .post(verifyToken, reviewControllers.postReview)
     .get(reviewControllers.getReviews);
 
 module.exports = router;
