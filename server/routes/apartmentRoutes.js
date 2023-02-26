e378784f11bcb188893577013007ac3ba92f098a
Md. Dipu <srabun.tangail@gmail.com>
2023-02-26 22:33:52 +0600

    [add] authorization for apartment post and delete request

--- server/routes/apartmentRoutes.js
+++ server/routes/apartmentRoutes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
 const apartmentControllers = require("../controllers/apartmentControllers");
+const authorization = require("../middleware/authorization");
 const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
 
 router.route("/")
-    .post(verifyToken, apartmentControllers.postApartment)
+    .post(verifyToken, authorization("admin"), apartmentControllers.postApartment)
     .get(apartmentControllers.getApartments);
 
 router.route("/:id")
     .get(apartmentControllers.getApartmentById)
-    .delete(verifyToken, apartmentControllers.deleteApartmentById);
+    .delete(verifyToken, authorization("admin"), apartmentControllers.deleteApartmentById);
 
 module.exports = router;
